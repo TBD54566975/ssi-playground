3a6b84f2685c4ba7bd3241be2940b6aac7afe003
Neal <groessler24@gmail.com>
2023-05-04 13:53:44 -0500

    adding webhook demo

--- index.js
+++ index.js
@@ -4,7 +4,66 @@ const fs = require('fs');
 const path = require('path');
 
 const proxy = httpProxy.createProxyServer({});
+
+let badges = []
+let webhooksRecieved = []
+
 const server = http.createServer((req, res) => {
+  handleWebhookRoutes(req, res)
+  handleIndexRoutes(req, res)
+});
+
+function handleWebhookRoutes(req, res) {
+  if (req.url === "/webhook" && req.method === "POST") {
+
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+
+    req.on('end', () => {
+      const jsonObject = JSON.parse(body);
+      const formattedJsonString = JSON.stringify(jsonObject, null, 2);
+      webhooksRecieved.push(formattedJsonString)
+
+      const credentialSubject = jsonObject?.data?.credential?.credentialSubject ?? undefined;
+
+      if (credentialSubject) {
+        let svgBadge = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100" height="100">
+        <circle cx="50" cy="50" r="45" stroke="black" stroke-width="3" fill="gold" />
+        <circle cx="50" cy="50" r="35" stroke="black" stroke-width="2" fill="white" />
+        <text x="50" y="55" font-family="Verdana" font-size="14" text-anchor="middle" alignment-baseline="central" fill="black">
+            VC
+        </text>
+        </svg>`;
+        badges.push(svgBadge)
+      }
+
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Webhook received and processed');
+    });
+  }
+
+  if (req.url === "/webhook" && req.method === "GET") {
+    fs.readFile(path.join(__dirname, 'webhook.html'), 'utf-8', (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading webhook.html');
+        return;
+      }
+
+      data = data.replace("{WEBHOOKS}", webhooksRecieved.join(" <hr/> "));
+      data = data.replace("{BADGES}", badges.join(" "));
+
+
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+
+      res.end(data);
+    });
+  }
+}
+
+function handleIndexRoutes(req, res) {
   if (req.url === '/') {
     fs.readFile(path.join(__dirname, 'index.html'), 'utf-8', (err, data) => {
       if (err) {
@@ -12,7 +71,7 @@ const server = http.createServer((req, res) => {
         res.end('Error loading index.html');
         return;
       }
-      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(data);
     });
   } else {
@@ -36,12 +95,12 @@ const server = http.createServer((req, res) => {
         contentType = 'image/svg+xml';
       }
 
-      res.writeHead(200, {'Content-Type': contentType});
+      res.writeHead(200, { 'Content-Type': contentType });
       res.end(data);
     });
   }
-});
+}
 
-server.listen(8081, () => {
+server.listen(8081, '0.0.0.0', () => {
   console.log('Server listening on http://localhost:8081');
 });
